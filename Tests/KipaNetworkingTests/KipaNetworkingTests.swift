ef9e87f5f1670f119a16fcd1fea73b77d1d61784
Daniel Prastiwa <daniel.prastiwa@akupeduli.org>
2022-02-21 13:24:48 +0700

    Clean up code

--- Tests/KipaNetworkingTests/KipaNetworkingTests.swift
+++ Tests/KipaNetworkingTests/KipaNetworkingTests.swift
@@ -1,11 +1,11 @@
 import XCTest
-@testable import KipaNetworking
+@testable import KipaNetworkTool
 
 final class KipaNetworkingTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(KipaNetworking().text, "Hello, World!")
+        //XCTAssertEqual(KipaNetworking().text, "Hello, World!")
     }
 }
